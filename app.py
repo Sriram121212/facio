01ec10196c25e2f999cca2a6664ed266d0578015
Sriram121212 <sriram958561@gmail.com>
2025-02-05 14:51:03 +0530

    test sakthi

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/sakthi')
+def hello_world():
+    return 'Hello, sakthi!'
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
